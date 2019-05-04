3b01ccd6b0b505bfbfddd222d60f0ed6151ff998
Jon Fackrell <admin@omnireference.com>
2019-05-03 23:12:39 -0600

    Added sidebar navigation

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,47 @@
 <?php
+namespace Elasticsearch;
 
 return [
-
+    'controllers' => [
+        'factories' => [
+            'Elasticsearch\Controller\Index' => Service\Controller\IndexControllerFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'admin' => [
+                'child_routes' => [
+                    'elasticsearch' => [
+                        'type' => 'Literal',
+                        'options' => [
+                            'route' => '/elasticsearch',
+                            'defaults' => [
+                                '__NAMESPACE__' => 'Elasticsearch\Controller',
+                                'controller' => 'Index',
+                                'action' => 'index',
+                            ],
+                        ],
+                        'may_terminate' => true,
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'navigation' => [
+        'AdminModule' => [
+            [
+                'label' => 'Elasticsearch',
+                'route' => 'admin/elasticsearch',
+                'resource' => 'Elasticsearch\Controller\Index',
+                'pages' => [
+                    [
+                        'label' => 'Import', // @translate
+                        'route' => 'admin/elasticsearch',
+                        'resource' => 'Elasticsearch\Controller\Index',
+                        'visible' => false,
+                    ],
+                ],
+            ],
+        ],
+    ],
 ];
